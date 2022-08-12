0409904c523c10e705ba89bd1a3d08ecd41ae53f
PH <anjibabubolla@Sriharsha-Mac.attlocal.net>
2022-08-11 22:06:50 -0500

    This is my initial commit

---
+++ first_program.py
@@ -0,0 +1,28 @@
+'''
+    write a program to print the following details of a person 
+    1. 
+    name 
+    age in years
+    height in centimeters
+    weight in kilograms
+    marital status 
+
+    2. 
+    height in feet
+
+'''
+
+first_name = "John"
+last_name = "Doe"
+age = 25
+
+height_cm = 180
+
+weight = 100
+
+marital_status = False
+
+height_ft = "6'2\""
+
+print(first_name + " " + last_name, age,
+      height_cm, weight, marital_status, height_ft)

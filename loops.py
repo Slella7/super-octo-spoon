c8bed66e4f415c214700297d0f84a68aa94e0449
PH <slella@live.com>
2022-08-29 20:08:02 -0500

    updated

--- loops.py
+++ loops.py
@@ -27,27 +27,20 @@ for i in range(100):
 print(dict_1)
 
 
-dict_1 = [{}, {}]
+dict_1 = [{}, {}, {}]
 count = 1
 for i in range(300):
     if (i % 2 != 0):
         continue
-    if (i < 100):
+    if (i <= 100):
         dict_1[0].update({count: i})
-    elif (i > 100 and i < 200):
+    elif (i > 100 and i <= 200):
         if i == 102:
             count = 1
         dict_1[1].update({count: i})
     else:
         if i == 202:
             count = 1
-        dict_1.append({count: i})
+        dict_1[2].update({count: i})
     count += 1
 print(dict_1)
-
-
-
-for outer_range in range(1,3):
-    for inner_range in range(1,100):
-        
-        print(outer_range, inner_range)

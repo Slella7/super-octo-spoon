779a123d08fbbf1eb75ca5b9025eaecbfdf651c4
PH <slella@live.com>
2022-08-30 21:12:06 -0500

    updated

--- loops.py
+++ loops.py
@@ -44,3 +44,29 @@ for i in range(300):
         dict_1[2].update({count: i})
     count += 1
 print(dict_1)
+
+
+# factorial of any number
+
+number = 9
+x = 1
+factorial = 1
+while number > x:
+    factorial *= number
+    number -= 1
+print(factorial)
+
+# number = 5
+# x = 1
+# factorial = 1
+# for y in range(number, 1, -1):
+#     factorial *= number
+
+# use logical operators and elif
+
+if factorial % 2 == 0 and factorial / 10 == int:
+    print("factorial is divisble by 10 and is even")
+elif factorial % 2 != 0 and factorial / 3 == int:
+    print("factorial is odd and divisible by 3")
+else:
+    print("none of the above conditions met")
